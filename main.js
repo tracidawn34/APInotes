260c62ae89523cc5f69e883b290fbca3294ef31e
unknown <tmcfall.prsvr@gmail.com>
2024-02-20 00:35:44 -0500

    got help still working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const URL = 'https://raw.githubusercontent.com/PersevereCode/react-rubric/master/src/rubric.json'
 
-const dogURL = 'https://dog.ceo/api/breeds/image/randohttps://dog.ceo/api/breeds/image/random'
+const dogURL = 'https://dog.ceo/api/breeds/image/random'
 
 const button = document.querySelector('button')
 
@@ -19,4 +19,70 @@ fetch(dogURL) //request the url
     console.log('response object acquired')
     console.log(object)
 })
+})
+
+
+//synchronous example
+
+console.log('first')
+console.log('second')
+console.log('third')
+
+//asynchronous example with callbacks
+
+//run the given callback after one second 
+function asyncWithCallback(callback){
+    setTimeout(callback, 1000)
+}
+
+asyncWithCallback(()=>{
+    console.log('first')
+    asyncWithCallback(()=>{
+        console.log('second')
+        asyncWithCallback(()=>{
+            console.log('third')
+        })
+    })
+})
+
+
+//asynchronous example with promises
+
+//returns a promise that resolve after one second
+function asyncWithPromise(){
+    return new Promise(resolve =>{
+setTimeout(resolve, 1000)
+    })
+}
+
+asyncWithPromise()
+.then(()=>{
+    console.log('hello world')
+    return asyncWithPromise()
+}) .then(()=>{
+    console.log('second request')
+    return asyncWithPromise()
+}) .then(()=>{
+    console.log('third request')
+})
+
+//asynchronous example with fetch
+
+const img = document.querySelector('img')
+
+//get a dog image
+fetch(dogURL)
+.then((rawResponse)=>{
+   return rawResponse.json()
+}) .then((dogImageLink)=>{
+    console.log(dogImageLink)
+    img.src = dogImageLink.message
+    return fetch(dogURL)
+})
+.then((rawResponse)=>{
+    return rawResponse.json()
+})
+.then((dogImageLink)=>{
+    console.log(dogImageLink)
+    img.src = dogImageLink.message
 })
